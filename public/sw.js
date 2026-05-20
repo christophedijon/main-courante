82259c110b4cc6ae787ab628b9016a8cd0a9c6e4
christophedijon <christopheinfo21@gmail.com>
2026-05-20 17:20:30 +0200

    Remove old Service Worker and reload

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,5 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    ).then(() => clients.claim())
-  );
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', () => {
+  self.registration.unregister();
+  clients.claim();
 });
